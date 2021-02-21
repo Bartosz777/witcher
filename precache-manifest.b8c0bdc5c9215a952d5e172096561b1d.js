1d9a45d95950337bc62cc4b3f0291f3e041ada2f
Bartek <bartekd001@gmail.com>
2021-02-21 19:49:18 +0100

    Updates

---
+++ precache-manifest.b8c0bdc5c9215a952d5e172096561b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91cb686aed143536b1a6",
+    "url": "/witcher/static/css/main.17038453.chunk.css"
+  },
+  {
+    "revision": "91cb686aed143536b1a6",
+    "url": "/witcher/static/js/main.6ee57dd1.chunk.js"
+  },
+  {
+    "revision": "dc64faa0ef6029edd567",
+    "url": "/witcher/static/js/runtime~main.cf540746.js"
+  },
+  {
+    "revision": "8722b5e13e56a82d00d0",
+    "url": "/witcher/static/js/2.19e651e6.chunk.js"
+  },
+  {
+    "revision": "85455cf67c80b6e9b4a0732fde150cdf",
+    "url": "/witcher/static/media/logo.85455cf6.svg"
+  },
+  {
+    "revision": "20e379ca72409dc94be90b59e543ef50",
+    "url": "/witcher/index.html"
+  }
+];
